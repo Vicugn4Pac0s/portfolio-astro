b177cb083ba163f136a11993c59ff1d1fd011bf2
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2022-11-12 18:55:29 +0900

    meta設定

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'YOSHIKI YAMADA';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = "Yoshiki's portfolio site";
+export const SITE_DESCRIPTION = 'YOSHIKIのポートフォリオサイト。独学でWebサイト制作を学び、Web制作会社に入社、数々の大手企業のWebサイト制作に携わってきました。煩わしいこと・めんどくさいことが大嫌いすぎて、快適なPC環境を求めて日々模索中。';
 export const MAIN_COLOR = '#99d3df'
