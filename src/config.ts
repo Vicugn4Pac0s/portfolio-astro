7a5a0c25202d3f39531fc547a77f9d0e4366d31d
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2022-11-12 15:45:40 +0900

    init

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'YOSHIKI YAMADA';
+export const SITE_DESCRIPTION = '';
+export const MAIN_COLOR = '#99d3df'
